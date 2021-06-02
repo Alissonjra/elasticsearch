5b9726d91d00a5221f3d7e8844775a60cbb5c74a
Alisson Jr <53801159+Alissonjra@users.noreply.github.com>
2021-06-02 14:05:23 -0300

    chore:  setup elasticsearch

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,22 @@
 import express from "express"
+import getClient from "./client/elasticSearch";
 
 const app = express()
+
+app.get('/',async (request,response)=>{
+  const client = getClient();
+
+  const result = await client.index({
+    index:'elastic_test',
+    type: 'type_elastic_test',
+    body:{
+      user: 'Alisson',
+      password: '123456ABC',
+      email: 'alissonjrananias@gmail.com'
+    }
+  })
+
+  return response.json(result)
+})
 
 app.listen(3333,()=> console.log("Running.."))
